c2d2e91597ce4dd19347136c4f87df189933aaa7
pontius <pontius@pnts.co>
2022-08-22 01:26:36 +0300

    get_the_terms issue with "1" ahead

--- test-theme/functions.php
+++ test-theme/functions.php
@@ -1,11 +1,11 @@
 <?php
 //variables
-var $ps_headlibs=[
+$ps_headlibs=[
     'bootstrap@5.2.0'=>'<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">',
     'jquery@3.6.0'=>'<script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>',
     'ionicons@2.0.1'=>'<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" crossorigin="anonymous" defer>'
 ];
-var $ps_footerlibs=[
+$ps_footerlibs=[
     'bootstrap@5.2.0'=>'<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous" defer></script>'
 ];
 //functions
@@ -14,13 +14,17 @@ function ps_printFunc($arr,$key){ //return callable to print value
     if (!!$arr && !!$key) $v=$arr[$key];
     return function() use ($v){ print($v);};
 }
-function ps_ioniconsLoad($version=null):string { //append Ionicons condition
-    if (!$version) return null;
-    if ( !have_posts() && is_search() ) print($ps_headlibs[$version]);
+function ps_ioniconsLoad($version=null) { //append Ionicons condition
+    $v='';
+    if ( !have_posts() && is_search() ) $v=$version;
+    return ps_printFunc($ps_headlibs,$version);
 }
 function ps_dateFilter($date,$format,$post) { //modify date output condition
-    if (  !$post->post_modified) return $date;
+    if (!$post->post_modified) return $date;
     return date_i18n($format, strtotime($post->post_modified_gmt), true);
+}
+function ps_updateTermsOutput($term_list,$taxonomy) { //modify category output
+    return get_the_term_list(the_ID(), $taxonomy, '', ', ', '');
 }
 function ps_taxonomyOrderFilter($query){ // change order of taxonomy query
     if (
@@ -28,14 +32,14 @@ function ps_taxonomyOrderFilter($query){ // change order of taxonomy query
         $query->is_main_query() &&
         !$query->is_tax('category') &&
         !$query->is_tax('tag')
-    ) $query->set('orderby'=>['modified'=>'ASC']);
+    ) $query->set('orderby',['modified'=>'ASC']);
 }
-function ps_404rw(){
+function ps_404rw(){ // enforce 404
     global $wp_query;
     $wp_query->set_404();
     status_header(404);
 }
-//hook actions
+//action hooks
 add_action( 'wp_head', ps_printFunc($ps_headlibs,'bootstrap@5.2.0'));
 add_action( 'wp_head', ps_printFunc($ps_headlibs,'jquery@3.6.0'), 2);
 add_action( 'wp_head', ps_ioniconsLoad('ionicons@2.0.1'));
--- test-theme/template-parts/post-content.php
+++ test-theme/template-parts/post-content.php
@@ -1,15 +1,22 @@
 <article class="post">
     <header>
-        <h1 class="post--title"><?php get_the_title(); add_filter( 'get_the_date', $ps_dateFilter, 10, 3);?></h1>
-        <p class="post--date"><?php get_the_date(‘M jS, Y’) ?></p>
-        <p class='post--categories'><?php the_terms( the_ID(), 'category', '', ', ' )?></p>
+        <h1 class="post--title"><?php
+        print(get_the_title());
+        //filter hooks
+        add_filter('get_the_date', 'ps_dateFilter', 10, 3);
+        add_filter('the_terms', 'ps_updateTermsOutput',10,2);
+        ?></h1>
+        <p class="post--date"><?php print(get_the_date('M jS, Y')) ?></p>
+        <p class='post--categories'><?php var_dump(get_the_terms(the_ID(), 'category'))?></p>
         <p class="post--author"><?php
             $ps_authorLastName=get_the_author_meta('last_name');
             $ps_authorFirstName=get_the_author_meta('first_name');
             $ps_authorRoles=get_user_by('id', get_the_author_meta('ID'))->roles;
+            var_dump($ps_authorRoles);
             print((!!$ps_authorLastName&&!!$ps_authorFirstName)? $ps_authorFirstName." ".$ps_authorLastName:get_the_author_meta('display_name'));
-            if(in_array(‘administrator’,$ps_authorRoles,true)) print('<span class="post--author-role">'.implode(', ',$ps_authorRoles).'</span>');
-            remove_filter( 'get_the_date', $ps_dateFilter, 10);
+            if(in_array('administrator',$ps_authorRoles,true)) print('<span class="post--author-role">'.implode(', ',$ps_authorRoles).'</span>');
+            remove_filter('get_the_date', 'ps_dateFilter', 10);
+            remove_filter('the_terms', 'ps_updateTermsOutput',10);
         ?></p>
     </header>
     <div class="post--content"><?php the_content()?></div>
